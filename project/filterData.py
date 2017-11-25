461bcec6b58097f03a2652fcdc3a23a4af3e9923
ImLemni <florian.billon@insa-lyon.fr>
2017-11-25 22:56:03 +0100

    Up until merge, filtered df created and saved

--- project/filterData.py
+++ project/filterData.py
@@ -7,7 +7,8 @@ def clean_title(title):
     removable = ['[', ']', 'dvd', 'vhs',
                  '(', ')', ',', '.', '!', '?', ':', ';']
     title = title.lower()
-    title = sub("\((.*film.*|.*movie.*)\)", "", title)
+    # title = sub("\((.*film.*|.*movie.*)\)", "", title)
+    title = sub("\(.*\)", "", title)
     title = sub("\s+", " ", title)
     title = sub("\s$", "", title)
     title = sub("^\s", "", title)
@@ -16,54 +17,55 @@ def clean_title(title):
     return title
 
 
-urlList = ["https://en.wikipedia.org/wiki/List_of_fiction_works_made_into_feature_films_(0%E2%80%939_and_A%E2%80%93C)",
-           "https://en.wikipedia.org/wiki/List_of_fiction_works_made_into_feature_films_(D%E2%80%93J)",
-           "https://en.wikipedia.org/wiki/List_of_fiction_works_made_into_feature_films_(K%E2%80%93R)",
-           "https://en.wikipedia.org/wiki/List_of_fiction_works_made_into_feature_films_(S%E2%80%93Z)"]
-dict_books_movies = getListOfBooks.get_dict_titles(urlList)
+def get_matching_product():
+    urlList = ["https://en.wikipedia.org/wiki/List_of_fiction_works_made_into_feature_films_(0%E2%80%939_and_A%E2%80%93C)",
+               "https://en.wikipedia.org/wiki/List_of_fiction_works_made_into_feature_films_(D%E2%80%93J)",
+               "https://en.wikipedia.org/wiki/List_of_fiction_works_made_into_feature_films_(K%E2%80%93R)",
+               "https://en.wikipedia.org/wiki/List_of_fiction_works_made_into_feature_films_(S%E2%80%93Z)"]
+    dict_books_movies = getListOfBooks.get_dict_titles(urlList)
 
-movies_titles = [item for sublist in dict_books_movies.values()
-                 for item in sublist]
-print(f"Number of movies from wikipedia : {len(movies_titles)}")
+    movies_titles = [item for sublist in dict_books_movies.values()
+                     for item in sublist]
+    print(f"Number of movies from wikipedia : {len(movies_titles)}")
 
-for i, mov in enumerate(movies_titles):
-    movies_titles[i] = clean_title(mov)
+    for i, mov in enumerate(movies_titles):
+        movies_titles[i] = clean_title(mov)
 
-movies_association = {el: [] for el in movies_titles}
+    movies_association = {el: [] for el in movies_titles}
 
-movies_metada_lines = data.read_data("meta_Movies_and_TV", 208321)
-for movie_line in movies_metada_lines:
-    if 'title' in movie_line:
-        title = clean_title(movie_line['title'])
-        if title in movies_association:
-            movies_association[title].append(movie_line)
+    movies_metada_lines = data.read_data("meta_Movies_and_TV", 208321)
+    for movie_line in movies_metada_lines:
+        if 'title' in movie_line:
+            title = clean_title(movie_line['title'])
+            if title in movies_association:
+                movies_association[title].append(movie_line)
 
-print(f"Number of movies selected: {sum([1 if len(v) > 0 else 0 for _, v in movies_association.items()])}")
+    print(f"Number of movies selected: {sum([1 if len(v) > 0 else 0 for _, v in movies_association.items()])}")
 
+    books_titles = [book for book in dict_books_movies]
+    print(f"Number of books from wikipedia : {len(books_titles)}")
 
-books_titles = [book for book in dict_books_movies]
-print(f"Number of books from wikipedia : {len(books_titles)}")
+    for i, book in enumerate(books_titles):
+        books_titles[i] = clean_title(book)
 
-for i, book in enumerate(books_titles):
-    books_titles[i] = clean_title(book)
+    books_association = {el: [] for el in books_titles}
 
-books_association = {el: [] for el in books_titles}
+    books_metada_lines = data.read_data("meta_Books", 2370585)
+    for book_line in books_metada_lines:
+        if 'title' in book_line:
+            title = clean_title(book_line['title'])
+            if title in books_association:
+                books_association[title].append(book_line)
+    print(f"Number of books selected: {sum([1 if len(v) > 0 else 0 for _, v in books_association.items()])}")
 
-books_metada_lines = data.read_data("meta_Books", 2370585)
-for book_line in books_metada_lines:
-    if 'title' in book_line:
-        title = clean_title(book_line['title'])
-        if title in books_association:
-            books_association[title].append(book_line)
-print(f"Number of books selected: {sum([1 if len(v) > 0 else 0 for _, v in books_association.items()])}")
-
-total_selected = 0
-for k, v in dict_books_movies.items():
-    associate = False
-    if len(books_association[clean_title(k)]) > 0:
-        for mov in v:
-            if len(movies_association[clean_title(mov)])>0:
-                associate = True
-                break
-    total_selected+= 1 if associate else 0
-print(f"Number of association book/movies : {total_selected}")
+    total_selected = 0
+    for k, v in dict_books_movies.items():
+        associate = False
+        if len(books_association[clean_title(k)]) > 0:
+            for mov in v:
+                if len(movies_association[clean_title(mov)]) > 0:
+                    associate = True
+                    break
+        total_selected += 1 if associate else 0
+    print(f"Number of association book/movies : {total_selected}")
+    return books_association,movies_association
---
+++ project/mergeReview.py
@@ -0,0 +1,52 @@
+from ada import data
+import filterData
+import pandas as pd
+
+
+def build_new_review_df(use_products, file, line_num):
+    reviews_lines = data.read_data(file, line_num)
+    i = 0
+    df = {}
+    for line in reviews_lines:
+        if line["asin"] in use_products:
+            df[i] = line
+            i += 1
+    return pd.DataFrame.from_dict(df, orient='index')
+
+
+books_match, movies_match = filterData.get_matching_product()
+# We build a dict as it is implemented as an hash table and we want a complexity
+# for checking presence of an element in O(1)
+
+
+books_product_id = {}
+for books in books_match.values():
+    for book in books:
+        books_product_id[book["asin"]] = book
+
+movies_product_id = {}
+for movies in movies_match.values():
+    for movie in movies:
+        movies_product_id[movie["asin"]] = movie
+
+print(f"We have {len(movies_product_id.keys())} distincts product for movies")
+print(f"We have {len(books_product_id.keys())} distincts product for books")
+
+books_new_df = build_new_review_df(books_product_id, "reviews_Books", 22507155)
+books_new_df.shape
+books_new_df.to_json("filtered_Books_reviews.json", orient='records')
+
+movies_new_df = build_new_review_df(
+    movies_product_id, "reviews_Movies_and_TV", 4607047)
+movies_new_df.shape
+movies_new_df.to_json("filtered_Movies_reviews.json", orient='records')
+
+books_meta_filtered_df = build_new_review_df(
+    books_product_id, "meta_Books", 2370585)
+books_meta_filtered_df.shape
+books_meta_filtered_df.to_json("filtered_Books_meta.json", orient='records')
+
+movies_meta_filetered_df = build_new_review_df(
+    movies_product_id, "meta_Movies_and_TV", 208321)
+movies_meta_filetered_df.shape
+movies_meta_filetered_df.to_json("filtered_Movies_meta.json",orient='records')
